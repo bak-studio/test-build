907225ba6d1e53047d889d2e465df69d95eda81c
bak-studio <96232017+bak-studio@users.noreply.github.com>
2022-02-17 15:37:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ const server = http.createServer((req, res) => {
     console.log('New connection')
     res.end('Hello Awesome 123')
 })
-const PORT = process.env.port || 8080
-server.listen(PORT, () => console.log('Listening'))
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => {
+    console.log('Listening')
+})
